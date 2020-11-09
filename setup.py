64825c1b76c046eaf450e637bf5d9c32d83baa7d
danuker <danuthaiduc@gmail.com>
2020-11-09 19:33:10 +0200

    Fix example comment; release v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="symreg",
-    version="0.0.1",
+    version="0.0.2",
     author="Dan Gheorghe Haiduc",
     author_email="danuthaiduc@gmail.com.com",
     description="A Symbolic Regression engine",
